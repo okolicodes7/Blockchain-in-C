f877c5940653ae2b15a83c47acd6538091dc07d7
okolicodes <okolifavour50@gmail.com>
2024-09-29 04:22:28 +0100

    Update:

--- Blockchain.c
+++ Blockchain.c
@@ -3,7 +3,8 @@
 #include <stdlib.h>
 #include <openssl/ssl.h>
 #include <time.h>
-#define constant MAX_NUMBER_OF_BLOCKS = 20;
+#include <stdbool.h>
+#define MAX_NUMBER_OF_BLOCKCHAIN 20
 
 // first step i took is to create a struct that defines what a Block comprises off onchain. 
 // the componenets of a Block 
@@ -16,11 +17,32 @@ typedef struct {
        int nonce;
 } Block;
 
+// I iniitialized an array called "blockchain" of Block type and passed in the [MAX_NUMBER_OF_BLOCKCHAIN]
+// Block blockchain[MAX_NUMBER_OF_BLOCKCHAIN];
+
+
 
 //open ssl hash function
 unsigned char *create_sha256(const unsigned char str[], unsigned char *buffer){
     SHA256(str, strlen((const char*)str), buffer);
     return buffer;
+}
+
+
+
+// second step as seen below is to be able to hash a block. 
+// for some weird reason in C, the code refused to compile as I wrote this function after the generate_block function so i had to move it up
+unsigned char* Hash_block(Block newblock) {
+    //what is going on here?  So.. basically I allocate a chunk of memory given as 1 * 1024 to the block_details variable which contains variables from the struct except from the hash variable.
+    unsigned char* block_details = malloc(sizeof(char) * 1024);
+
+// then i use sprintf() to store the block's information as a formatted string into the block_details.
+      sprintf((char*)block_details, "%zu, %s, %d, %d,\n", newblock.index, (char*)newblock.previousHash, newblock.timestamp, newblock.nonce);
+      // allocate another memory space to the buffer ( 1 * 256 bits(32 bytes))
+      unsigned char* buffer = malloc(sizeof(char) * SHA256_DIGEST_LENGTH);
+      // proceed to call the  create_sha256 function to create the blocks hash. Encountered a lot of issues down here as I did not cast the input parameters properly and my openssl create_sha256 function was not defined in the code so i had to define it properly above in line 20 (I initially only declared the function signature as is done with solidity)
+     block_details = create_sha256((const unsigned char*) block_details, (unsigned char*) buffer);
+  return buffer;
 }
 
 
@@ -30,27 +52,79 @@ Block generate_block(Block oldblock, int nonce){
     Block newblock;
 
     //Then I proceed to setting the elements of the newblock struct. Which is basically the componenets of a Block Struct
-    newblock.index = oldblock.index + 1;
-    // printf("the index is %ld\n", newblock.index);
-  newblock.timestamp = 
-  newblock.nonce = nonce;
-  newblock.hash ;
-  newblock.previousHash;
+     newblock.index = oldblock.index + 1;
+     newblock.timestamp = time(NULL);
+     newblock.nonce = nonce;
+     newblock.hash = Hash_block(newblock); 
+     newblock.previousHash = oldblock.hash;
+     return newblock;
 }
 
+// fourth step 
+ bool validate_block(Block newblock, Block oldblock){
+    if(newblock.index != oldblock.index + 1){
+        printf("BLOCK NOT VALID\n");
+        return false;
+    }
+    if(oldblock.hash != newblock.previousHash){
+        printf("HASH NOT VALID\n");
+        return false;
+    }
+    if(newblock.hash != Hash_block(newblock)){
+        printf("INVALID BLOCK\n");
+        return false;
+    }
+return true;
+ }
 
-// second step as seen below is to be able to hash a block. 
-unsigned char* Hash_block(Block newblock) {
-    //what is going on here?  So.. basically I allocate a chunk of memory given as 1 * 1024 to the block_details variable which contains variables from the struct except from the hash variable.
-    unsigned char* block_details = malloc(sizeof(char) * 1024);
-// then i use sprintf() to store the block's information as a formatted string into the block_details.
-      sprintf("%zu, %s, %d, %d,\n", (char*)block_details, newblock.index, newblock.previousHash, newblock.timestamp, newblock.nonce);
-      // allocate another memory space to the buffer ( 1 * 256 bits(32 bytes))
-      unsigned char* buffer = malloc(sizeof(char) * SHA256_DIGEST_LENGTH);
-      // proceed to call the  create_sha256 function to create the blocks hash. Encountered a lot of issues down here as I did not cast the input parameters properly and my openssl create_sha256 function was not defined in the code so i had to define it properly above in line 20 (I initially only declared the function signature as is done with solidity)
-     block_details = create_sha256((const unsigned char*) block_details, (unsigned char*) buffer);
-  return buffer;
+/*
+// fifth step
+ Block replacing_chain(Block newblockchain[MAX_NUMBER_OF_BLOCKCHAIN], Block oldblockchain[MAX_NUMBER_OF_BLOCKCHAIN]){
+  int new_length;
+   int old_length;
+    
+  if(newlength > oldlength){
+     printf("VALID CHAIN\n");
+     return newblockchain;
+;
+  }
+  else {
+     printf("VALID CHAIN\n");
+    return oldblockchain;
+   ;
+  }
+     
+ }
+ */
+// step six (proceeds to create a print function that prints the all the elements in the Block struct)
+void printblock(Block block) {
+    printf("INDEX: %zu\n", block.index);
+    printf("TIMESTAMP: %d\n", block.timestamp);
+    printf("NONCE: %d\n", block.nonce);
+    if(block.previousHash == NULL){
+    printf("PREV HASH: NULL\n");
+} else {
+    printf("PREV HASH: ");
+    for(int i = 0; i < SHA256_DIGEST_LENGTH; i++){
+        if(block.previousHash != NULL){
+            printf("%02X", block.previousHash[i]);
+        }
+    }
+    printf("\n");
 }
+ if(block.hash == NULL){
+    printf("HASH: NULL\n");
+} else {  
+    for(int i = 0; i < SHA256_DIGEST_LENGTH; i++){
+        if(block.previousHash != NULL){
+            printf("%02X", block.previousHash[i]);
+        }
+}
+  printf("\n");
+
+}
+}
+   
 
 
 
@@ -63,9 +137,19 @@ unsigned char* Hash_block(Block newblock) {
 
 int main()
 {
- // Block block1 =  generate_block(Block oldblock, int nonce +1);
-    printf("My Blockchain project!\n");
+   // Block blockchain[MAX_NUMBER_OF_BLOCKCHAIN];
+   // initializing the different parameters passed in the block struct
+   
+   Block genesisblock;
+   genesisblock.index = 0;
+   genesisblock.timestamp = time(NULL);
+   genesisblock.nonce = 0;
+   genesisblock.previousHash = NULL;
+   genesisblock.hash = Hash_block(genesisblock);
+   printblock(genesisblock);
+   // printf("My Blockchain project!\n");
     return 0;
+
 }
 
 
